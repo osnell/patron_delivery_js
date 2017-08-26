bd6da47e25ff29621be693f44b97db5a65db3964
osnell <oliversnell9@gmail.com>
2017-08-26 15:04:35 -0400

    commit test

--- Terrain.js
+++ Terrain.js
@@ -1,5 +1,12 @@
+const TILE_WIDTH = 120;
+const TILE_HEIGHT = 80;
+
 
 function Terrain() {
+    var tiles = new Array(12);
+    for (var i=0; i<tiles.length; i++){
+        tiles[i] = new Array(8);
+    }
 
     this.draw = function () {
         drawRect(0,0, canvas.width, canvas.height, 'black');
